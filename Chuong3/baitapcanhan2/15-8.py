13a3ca18267cfdea671936dc3ba1cbf7164c7f19
akhoa6204 <126770761+akhoa6204@users.noreply.github.com>
2023-03-06 17:12:11 +0700

    Update 15-8.py

--- Chuong3/baitapcanhan2/15-8.py
+++ Chuong3/baitapcanhan2/15-8.py
@@ -4,16 +4,5 @@ i = int(input())
 for a in range(1, i+1):
     print(a * (n+h), end=' ', sep=' ')
     print()
-# for a in range(1, i+1):
-#     print(n*a, end=" ")
-# print()
-# for a in range(i-1, 0, -1):
-#     print(n*a, end=" ")
-#     print()
-   
-
-
-
-# chưa cách nhau 1 dấu cách
 
    
